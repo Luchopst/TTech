d8b6b1bf117700af545b9d778f6307dc8006bb45
Luciano Ponce <poncestigaardluciano@gmail.com>
2024-12-16 21:07:28 -0300

    se agrega doc js

--- Script/script.js
+++ Script/script.js
@@ -1,43 +1,22 @@
-let primerNumeroIngresado = parseFloat(prompt("Introduce el primer número:"));
+// Obtener todos los botones de ver descripción
+const botonesDescripcion = document.querySelectorAll(".ver-descripcion");
 
-if (!isNaN(primerNumeroIngresado)) {
-  let segundoNumeroIngresado = parseFloat(
-    prompt("Introduce el segundo número:")
-  );
-  if (!isNaN(segundoNumeroIngresado)) {
-    let totalSuma = primerNumeroIngresado + segundoNumeroIngresado;
-    let totalResta = primerNumeroIngresado - segundoNumeroIngresado;
-    let totalMultiplicacion = primerNumeroIngresado * segundoNumeroIngresado;
-    let totalDivision = primerNumeroIngresado / segundoNumeroIngresado;
-    console.log("el resultado de la SUMA es: " + "[" + totalSuma + "]");
-    console.log("el resultado de la RESTA es: " + "[" + totalResta + "]");
-    console.log(
-      "el resultado de la MULTIPLICACION es: " + "[" + totalMultiplicacion + "]"
-    );
-    console.log("el resultado de la DIVISION es: " + "[" + totalDivision + "]");
-  }
-  if (valorDado > totalSuma) {
-    console.log("el resultado de la suma es menor que Valor Dado");
-  } else {
-    console.log("La suma es mayor que el Valor Dado");
-  }
-  if (valorDado > totalResta) {
-    console.log("La RESTA es menor que el Valor Dado");
-  } else {
-    console.log("La RESTA es mayor que el Valor Dado");
-  }
-  if (valorDado > totalMultiplicacion) {
-    console.log("La MULTIPLICACION es Menor que el Valor Dado");
-  } else {
-    console.log("La MULTIPLICACION es Mayor que El Valor Dado");
-  }
-  if (valorDado > totalDivision) {
-    console.log("La DIVISION es Menor que el Valor Dado");
-  } else {
-    console.log("La DIVISION es Mayor que el Valor Dado");
-  }
-} else {
-  console.log("El valor ingresado no es VALIDO");
-}
-let valorDado = 150;
-console.log("Gracias Vuelva Pronto!!!");
+// Agregar un evento 'click' a cada uno de los botones
+botonesDescripcion.forEach((boton) => {
+  boton.addEventListener("click", function () {
+    // Obtener el contenedor del producto que fue clickeado
+    const producto = boton.closest(".producto");
+
+    // Obtener la descripción
+    const descripcion = producto.querySelector(".descripcion");
+
+    // Alternar la visibilidad de la descripción
+    if (descripcion.style.display === "none") {
+      descripcion.style.display = "block"; // Mostrar descripción
+      boton.textContent = "Ocultar descripción"; // Cambiar el texto del botón
+    } else {
+      descripcion.style.display = "none"; // Ocultar descripción
+      boton.textContent = "Ver descripción"; // Restablecer el texto del botón
+    }
+  });
+});
